d6ce8e40567a00b08814c7d10488ba370ed47c5c
zhaorunze2018 <39620720+zhaorunze2018@users.noreply.github.com>
2018-05-31 18:12:00 +0800

    Update validate.py

--- ball_in_box/validate.py
+++ ball_in_box/validate.py
@@ -27,9 +27,9 @@ def validate(circles, blockers):
                     return False
 
     # Is circle good for each other?
-    i=len(circles)
-    for m in range(i-1):
-        for n in range(m+1,i):
+    lenth=len(circles)
+    for m in range(lenth-1):
+        for n in range(m+1,lenth):
             x1 = circles[m][0]
             y1 = circles[m][1]
             r1 = circles[m][2]
