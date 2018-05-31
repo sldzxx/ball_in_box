3c5893f25c0754f70decfe39b122689185e7fb71
Mirai <wr568367887@gmail.com>
2018-05-31 14:02:25 +0800

    Add files via upload

--- ball_in_box/ballinbox.py
+++ ball_in_box/ballinbox.py
@@ -1,9 +1,20 @@
 import math
-import random
-from .validate import validate
+import validate as val
 
 __all__ = ['ball_in_box']
 
+def divide(n):
+    hen = 0.02
+    pointList = []
+    for i in range(n):
+        for j in range(n):
+            if (-1)+i*hen < 1 and (-1)+j*hen < 1:
+                pointList.append( ((-1)+i*hen, (-1)+j*hen) )
+    return pointList
+
+def getDistance(center_1,center_2):
+    return math.sqrt((center_1[0]-center_2[0])**2+(center_1[1]-center_2[1])**2)
+    
 def ball_in_box(m=5, blockers=[(0.5, 0.5), (0.5, -0.5), (0.5, 0.3)]):
     """
     m is the number circles.
@@ -11,22 +22,23 @@ def ball_in_box(m=5, blockers=[(0.5, 0.5), (0.5, -0.5), (0.5, 0.3)]):
     
     This returns a list of tuple, composed of x,y of the circle and r of the circle.
     """
-
-    # The following is an example implementation.
-    circles = []
-    for circle_index in range(m):
-
-        x = random.random()*2 - 1
-        y = random.random()*2 - 1
-        r = random.random()*0.1
-
-        circles.append((x, y, r))
-        while not validate(circles, blockers):
-            x = random.random()*2 - 1
-            y = random.random()*2 - 1
-            r = random.random()*0.1
-            circles[circle_index] = (x, y, r)
-
-        circle_index += 1
+    allPoints = divide(100)
+    circleList=[]
     
-    return circles
+    for i in range(m):
+        mmaxC=[0,0,0]
+        for point in allPoints:
+            mminR = min(map(abs,[point[0]-1,point[0]+1,point[1]-1,point[1]+1]))
+            for block in blockers:
+                mminR = min(mminR,getDistance(point,block))
+            for circle in circleList:
+                mminR = min(mminR,getDistance(point,circle)-circle[2])
+            if  mminR > mmaxC[2]:
+                mmaxC[0] = point[0]
+                mmaxC[1] = point[1]
+                mmaxC[2] = mminR
+
+        circleList.append(mmaxC)   
+        allPoints = list(filter(lambda p: getDistance(p,mmaxC) > mmaxC[2] , allPoints))       
+
+    return circleList
